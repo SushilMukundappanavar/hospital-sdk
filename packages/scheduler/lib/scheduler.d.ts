cb3afe59a5f0074f69296b2854b1d3cfa1897edc
SushilMukundappanavar <tmsushil@gmail.com>
2019-02-14 03:03:30 -0800

    first commit

---
+++ packages/scheduler/lib/scheduler.d.ts
@@ -0,0 +1,3 @@
+export declare class Scheduler {
+    schedule(patientID: any, doctorID: any, date: any): {};
+}
---
+++ packages/scheduler/lib/scheduler.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Scheduler {
+    schedule(patientID, doctorID, date) {
+        return {};
+    }
+}
+exports.Scheduler = Scheduler;
+//# sourceMappingURL=scheduler.js.map
